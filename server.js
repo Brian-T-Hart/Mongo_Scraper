383f531df1c8ca9ef055c2fd7388a88a34ee14d1
Brian Hart <brianhart@dhcp-v088-235.mobile.uci.edu>
2017-10-21 15:06:24 -0700

    added var PORT

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.Promise = Promise;
 
 // Initialize Express
 var app = express();
-
+var PORT = process.env.PORT || 8080;
 // Use morgan and body parser with our app
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
@@ -169,8 +169,6 @@ app.post("/articles/:id", function(req, res) {
   });
 });
 
-
-// Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server connected. Listening on port: " + PORT);
 });
